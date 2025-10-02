2a9bc58d888750fa43d0c2099b3c8ecadba79cf1
nhassl3 <matvey.kvasov.05@mail.ru>
2025-10-02 11:15:30 +0300

    🙈 Update .gitignore file

---
+++ internals/app/grpcapp/grpc.go
@@ -0,0 +1,26 @@
+package grpcapp
+
+import (
+	"log/slog"
+
+	authgrpc "github.com/nhassl3/sso-app/internals/grpc/auth"
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+func NewApp(log *slog.Logger, port int) *App {
+	gRPCServer := grpc.NewServer()
+
+	authgrpc.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
