5f4b20ee8738c0aca20c790675e9a5e22d802256
nhassl3 <matvey.kvasov.05@mail.ru>
2025-10-05 11:57:29 +0300

    🎉 Call service (main file)

--- cmd/ssoapp/sso.go
+++ cmd/ssoapp/sso.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/nhassl3/sso-app/internals/app"
+	"github.com/nhassl3/sso-app/internals/config"
+	"github.com/nhassl3/sso-app/internals/lib/logger/handlers/slogpretty"
+)
+
+const (
+	_              = iota
+	EnvLocal       // local
+	EnvDevelopment // development
+	EnvProduction  // production
+)
+
+var (
+	cfg *config.Config
+	log *slog.Logger
+)
+
+func init() {
+	cfg = config.MustLoad()
+	log = setupLogger(cfg.Env)
+}
+
+func main() {
+	application := app.NewApp(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+
+	go application.GRPCServer.MustStart()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	sign := <-stop
+
+	application.GRPCServer.Stop()
+	// Stop every service and components (databases for ex) separately
+
+	log.Info("Application stopped", slog.String("sign", sign.String()))
+}
+
+func setupLogger(env uint8) *slog.Logger {
+	var level slog.Leveler
+	switch env {
+	case EnvLocal, EnvDevelopment:
+		level = slog.LevelDebug
+	case EnvProduction:
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+	return slog.New(slogpretty.NewPrettyHandler(os.Stdout, slogpretty.PrettyHandlerOptions{
+		SlogOpts: slog.HandlerOptions{
+			Level: level,
+		},
+	}))
 }
